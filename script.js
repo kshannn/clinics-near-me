ea94ca3f4fa9c4ef04535bc0c0949ad333430960
kshannn <77404139+kshannn@users.noreply.github.com>
2021-05-15 17:19:42 +0800

    Added checkbox toggle for testing

--- script.js
+++ script.js
@@ -19,8 +19,21 @@ window.addEventListener("DOMContentLoaded", async function () {
     let pharmacyResponse = await axios.get("geojson/retail-pharmacy.geojson");
     let pharmacyData = pharmacyResponse.data
 
+    // Create pharmacy cluster layer
     let pharmacyClusterLayer = L.markerClusterGroup();
     pharmacyClusterLayer.addTo(map);
+
+    // Create pharmacy layer group
+    // let pharmacyGroup = L.layerGroup();
+    // pharmacyGroup.addTo(map);
+
+
+    //Create baselayer/overlays(s) and add to map
+    // let baseLayers = {
+    //     "Pharmacies": pharmacyGroup
+    // }
+
+    // L.control.layers(baseLayers).addTo(map);
 
     for (let pharmacy of pharmacyData.features) {
 
@@ -30,12 +43,13 @@ window.addEventListener("DOMContentLoaded", async function () {
 
         let pharmacyLocation = pharmacy.geometry.coordinates
         L.marker([pharmacyLocation[1], pharmacyLocation[0]]).addTo(pharmacyClusterLayer).bindPopup(pharmacyName)
+        // L.marker([pharmacyLocation[1], pharmacyLocation[0]]).addTo(pharmacyGroup);
     }
 
 })
 
 
-
+// Main search box disappear when clicked away
 document.querySelector("#contentContainer").addEventListener("click",function(event){
     if (document.querySelector("#content").contains(event.target)){
         // pass
@@ -43,8 +57,14 @@ document.querySelector("#contentContainer").addEventListener("click",function(ev
         // hide div
         document.querySelector("#content").style.display="none";
         document.querySelector("#contentContainer").style.display="none";
-        document.querySelector("#map").style.zIndex = "1";
+        document.querySelector("#map").style.zIndex = "0";
         
     }
     
 })
+
+// Toggle buttons
+// let pharmacyBtn = document.querySelector("#togglePharmacyBtn")
+// pharmacyBtn.addEventListener("click", function{
+
+// })
