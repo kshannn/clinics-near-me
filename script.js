444b0201927521a394a156f310da620733fbd717
kshannn <77404139+kshannn@users.noreply.github.com>
2021-05-15 21:02:28 +0800

    Added clinic locations

--- script.js
+++ script.js
@@ -15,56 +15,81 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 window.addEventListener("DOMContentLoaded", async function () {
 
-    // Pharmacy locations
-    let pharmacyResponse = await axios.get("geojson/retail-pharmacy.geojson");
-    let pharmacyData = pharmacyResponse.data
+    // Clinic locations
+
+    let clinicsResponse = await axios.get("geojson/chas-clinics.geojson");
+    let clinicsData = clinicsResponse.data
+
 
     // Create pharmacy cluster layer
-    let pharmacyClusterLayer = L.markerClusterGroup();
-    pharmacyClusterLayer.addTo(map);
+    let clinicClusterLayer = L.markerClusterGroup();
+    clinicClusterLayer.addTo(map);
 
-    // Create pharmacy layer group
-    // let pharmacyGroup = L.layerGroup();
-    // pharmacyGroup.addTo(map);
+    for (let clinic of clinicsData.features) {
+        let clinicName = clinic.properties.Description.split("<td>")
+        clinicName = clinicName[2].split("</td>")
+        clinicName = clinicName[0]
 
-
-    //Create baselayer/overlays(s) and add to map
-    // let baseLayers = {
-    //     "Pharmacies": pharmacyGroup
-    // }
-
-    // L.control.layers(baseLayers).addTo(map);
-
-    for (let pharmacy of pharmacyData.features) {
-
-        let pharmacyName = pharmacy.properties.Description.split("<td>")
-        pharmacyName = pharmacyName[7].split("</td>")
-        pharmacyName = pharmacyName[0]
-
-        let pharmacyLocation = pharmacy.geometry.coordinates
-        L.marker([pharmacyLocation[1], pharmacyLocation[0]]).addTo(pharmacyClusterLayer).bindPopup(pharmacyName)
-        // L.marker([pharmacyLocation[1], pharmacyLocation[0]]).addTo(pharmacyGroup);
+        let clinicLocation = clinic.geometry.coordinates
+        L.marker([clinicLocation[1], clinicLocation[0]]).addTo(clinicClusterLayer).bindPopup(clinicName)
     }
+
+
+
+
+
+
+
+    // // Pharmacy locations
+    // let pharmacyResponse = await axios.get("geojson/retail-pharmacy.geojson");
+    // let pharmacyData = pharmacyResponse.data
+
+    // // Create pharmacy cluster layer
+    // let pharmacyClusterLayer = L.markerClusterGroup();
+    // pharmacyClusterLayer.addTo(map);
+
+    // // Create pharmacy layer group
+    // // let pharmacyGroup = L.layerGroup();
+    // // pharmacyGroup.addTo(map);
+
+
+    // //Create baselayer/overlays(s) and add to map
+    // // let baseLayers = {
+    // //     "Pharmacies": pharmacyGroup
+    // // }
+
+    // // L.control.layers(baseLayers).addTo(map);
+
+    // for (let pharmacy of pharmacyData.features) {
+
+    //     let pharmacyName = pharmacy.properties.Description.split("<td>")
+    //     pharmacyName = pharmacyName[7].split("</td>")
+    //     pharmacyName = pharmacyName[0]
+
+    //     let pharmacyLocation = pharmacy.geometry.coordinates
+    //     L.marker([pharmacyLocation[1], pharmacyLocation[0]]).addTo(pharmacyClusterLayer).bindPopup(pharmacyName)
+    //     // L.marker([pharmacyLocation[1], pharmacyLocation[0]]).addTo(pharmacyGroup);
+    // }
 
 })
 
 
 // Main search box disappear when clicked away
-document.querySelector("#contentContainer").addEventListener("click",function(event){
-    if (document.querySelector("#content").contains(event.target)){
+document.querySelector("#contentContainer").addEventListener("click", function (event) {
+    if (document.querySelector("#content").contains(event.target)) {
         // pass
     } else {
         // hide div
-        document.querySelector("#content").style.display="none";
-        document.querySelector("#contentContainer").style.display="none";
+        document.querySelector("#content").style.display = "none";
+        document.querySelector("#contentContainer").style.display = "none";
         document.querySelector("#map").style.zIndex = "0";
-        
+
     }
-    
+
 })
 
 // Toggle buttons
 // let pharmacyBtn = document.querySelector("#togglePharmacyBtn")
-// pharmacyBtn.addEventListener("click", function{
+// pharmacyBtn.addEventListener("change", function{
 
 // })
